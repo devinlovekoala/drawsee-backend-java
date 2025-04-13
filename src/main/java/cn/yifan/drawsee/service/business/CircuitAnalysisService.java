8e8d7e8d56658775c0d14a02eb536cfb451eb131
devin <buptdevin@gmail.com>
2025-04-13 10:21:38 +0800

    4.12 Devin修正后端AI任务工作流系列类实现

---
+++ src/main/java/cn/yifan/drawsee/service/business/CircuitAnalysisService.java
@@ -0,0 +1,17 @@
+package cn.yifan.drawsee.service.business;
+
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @FileName CircuitAnalysisService
+ * @Description 电路分析服务
+ * @Author devin
+ * @date 2025-04-13 21:38
+ **/
+
+@Service
+@Slf4j
+public class CircuitAnalysisService {
+    // 电路分析服务相关方法
+}
